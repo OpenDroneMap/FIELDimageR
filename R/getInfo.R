5c22684aeb0f87a2426f4de6dad5cb37e66c9f9a
Filipe Matias <11415688+filipematias23@users.noreply.github.com>
2020-01-27 10:35:34 -0600

    Update getInfo.R

--- R/getInfo.R
+++ R/getInfo.R
@@ -4,13 +4,13 @@ getInfo<-function(mosaic,fieldShape,fun = "mean",plot=F,buffer=NULL,n.core=NULL,
   mosaic <- stack(mosaic)
   num.band<-length(mosaic@layers)
   print(paste( "Extracting: ", num.band, " layers.", sep = ""))
-  if(n.core>detectCores()){stop(paste(" 'n.core' must be less than ",detectCores(),sep = ""))}
   print(paste("You can speed up this step using n.core=", detectCores(), " or less.", sep = ""))
   CropPlot <- crop(x = mosaic, y = fieldShape)
   if(is.null(n.core)){
     plotValue <- extract(x = CropPlot, y = fieldShape, fun = eval(parse(text = fun)),
                          buffer = buffer, na.rm = T, df = T, ...)}
   if(!is.null(n.core)){
+    if(n.core>detectCores()){stop(paste(" 'n.core' must be less than ",detectCores(),sep = ""))}
   cl <- makeCluster(n.core, output="")
   registerDoParallel(cl)
   plotValue <- foreach(i=1:length(fieldShape), .packages= c("raster"), .combine = rbind) %dopar% {
