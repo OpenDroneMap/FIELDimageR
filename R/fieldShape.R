616b32523b8e52e1c01eb150f05654f5696e1558
Filipe Matias <11415688+filipematias23@users.noreply.github.com>
2020-02-03 11:11:00 -0600

    Update fieldShape.R

--- R/fieldShape.R
+++ R/fieldShape.R
@@ -1,12 +1,15 @@
-fieldShape<-function(mosaic,ncols=10,nrows=10,nPoint=4,fieldMap=NULL,fieldData=NULL,ID=NULL,plot=T){
+fieldShape<-function(mosaic,ncols=10,nrows=10,nPoint=4,fieldMap=NULL,fieldData=NULL,ID=NULL,plot=T,fast.plot=F){
   source(file=system.file("extdata","RGB.rescale.R", package = "FIELDimageR", mustWork = TRUE))
   mosaic <- stack(mosaic)
   num.band<-length(mosaic@layers)
   print(paste(num.band," layers available", sep = ""))
   if(nPoint<4|nPoint>50){stop("nPoint must be >= 4 and <= 50")}
   par(mfrow=c(1,2))
+  if(fast.plot){
+      raster::plot(mosaic[[1]], col=grey(1:100/100), axes=FALSE, box=FALSE, legend=FALSE)}
+    if(!fast.plot){
   if(num.band>2){plotRGB(RGB.rescale(mosaic,num.band=3), r = 1, g = 2, b = 3)}
-  if(num.band<3){raster::plot(mosaic, axes=FALSE, box=FALSE)}
+  if(num.band<3){raster::plot(mosaic, axes=FALSE, box=FALSE)}}
   print(paste("Select ",nPoint," points at the corners of field of interest in the plots space.",sep = ""))
   c1<-NULL
   for(i in 1:nPoint){
@@ -24,13 +27,17 @@ fieldShape<-function(mosaic,ncols=10,nrows=10,nPoint=4,fieldMap=NULL,fieldData=N
   grid <- raster(f1,nrows=nrows, ncols=ncols,crs = proj4string(r))
   fieldShape <- rasterToPolygons(grid)
   if(plot){
+    if(fast.plot){
+      raster::plot(r[[1]], col=grey(1:100/100), axes=FALSE, box=FALSE, legend=FALSE)
+      sp::plot(fieldShape, add = T)}
+    if(!fast.plot){
     if(num.band>2){
       plotRGB(RGB.rescale(r,num.band=3), r = 1, g = 2, b = 3)
       sp::plot(fieldShape, add = T)
     }
     if(num.band<3){
       raster::plot(r, axes=FALSE, box=FALSE)
-      sp::plot(fieldShape, add = T)}}
+      sp::plot(fieldShape, add = T)}}}
   fieldShape@data<-data.frame(fieldID=as.character(seq(1,length(fieldShape$layer))))
 if(!is.null(fieldMap)){fieldShape@data<-data.frame(PlotName=as.character(c(t(fieldMap))))}
 if(!is.null(fieldData)){
