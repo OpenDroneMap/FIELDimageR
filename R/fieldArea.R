52d776ff9fd4acb9bf42ce296f895081be6e4829
Filipe Matias <11415688+filipematias23@users.noreply.github.com>
2020-05-28 08:53:07 -0500

    Update fieldArea.R

--- R/fieldArea.R
+++ R/fieldArea.R
@@ -5,12 +5,12 @@ fieldArea<-function(mosaic, areaValue=0, fieldShape=NULL, n.core=NULL, plot=T){
   print(paste("You can speed up this step using n.core=", detectCores(), " or less.", sep = ""))
   if(num.band>1){stop("Only mask mosaic with values of 1 and 0 can be evaluated, please use the mask output from fieldMask()")}
   if(!areaValue%in%c(1,0)){stop("The value must be 1 or 0 to represent the objects in the mask mosaic, please use the mask output from fieldMask()")}
-  pc <- function(x, p){return( length(x[x == p]) / length(x[!is.na(x)]) )}
+  pc <- function(x, p){return( data.frame(objArea=(length(x[x == p])/length(x[!is.na(x)])),objNumCell=length(x[x == p]),naNumCell=length(x[!is.na(x)])) )}
   if(is.null(fieldShape)){
     print("Evaluating the object area percentage for the whole image...")
     porarea<-pc(x=mosaic, p=areaValue)
-    print(paste("The percentage of object area is ",100*(round(porarea,2)),"%",sep = ""))
-    Out<-porarea
+    print(paste("The percentage of object area is ",100*(round(porarea$objArea,2)),"%",sep = ""))
+    Out<-list(areaPorcent=porarea)
   }
   if(plot){raster::plot(mosaic, col=grey(1:100/100), axes=FALSE, box=FALSE)}
   if(!is.null(fieldShape)){
@@ -19,7 +19,7 @@ fieldArea<-function(mosaic, areaValue=0, fieldShape=NULL, n.core=NULL, plot=T){
     if (is.null(n.core)) {
       extM <- extract(x = mosaic, y = fieldShape)
       names(extM) <- 1:length(fieldShape)
-      porarea <- unlist(lapply(extM, pc, p = areaValue))
+      porarea <- as.data.frame(do.call(rbind,lapply(extM, pc, p = areaValue)))
     }
     if (!is.null(n.core)) {
       if(n.core>detectCores()){stop(paste(" 'n.core' must be less than ",detectCores(),sep = ""))}
@@ -32,9 +32,9 @@ fieldArea<-function(mosaic, areaValue=0, fieldShape=NULL, n.core=NULL, plot=T){
           extract(x = CropPlot, y = single)
         }
       names(extM) <- 1:length(fieldShape)
-      porarea <- unlist(lapply(extM, function(x){pc(as.numeric(x[[1]]),p = areaValue)}))
+      porarea <- as.data.frame(do.call(rbind,lapply(extM, function(x){pc(as.numeric(x[[1]]),p = areaValue)})))
     }
-    fieldShape@data$areaPorcent=porarea
+    fieldShape@data<-cbind.data.frame(fieldShape@data,porarea)
     Out<-list(areaPorcent=porarea, fieldShape=fieldShape)
     if(plot){sp::plot(fieldShape, add = T)}
   }
